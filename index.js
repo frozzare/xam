a322f1156f852a4f09d0ba59c4c939fe4e6624fe
Fredrik Forsmo <fredrik.forsmo@gmail.com>
2014-03-25 16:14:18 +0100

    fixed open issue with missing dash

--- index.js
+++ index.js
@@ -1,2 +1,2 @@
 #!/usr/bin/env node
-require('child_process').exec('open -n /Applications/Xamarin\ Studio.app ' + require('path').resolve(process.cwd(), process.argv[2] || ''))
+require('child_process').exec('open -n /Applications/Xamarin\\ Studio.app ' + require('path').resolve(process.cwd(), process.argv[2] || ''))
